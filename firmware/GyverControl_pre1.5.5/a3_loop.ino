395c28e730e9e47b230f772d9f7d4d500371cb55
Alex <beragumbo@ya.ru>
2020-02-26 14:40:06 +0300

    add

---
+++ firmware/GyverControl_pre1.5.5/a3_loop.ino
@@ -0,0 +1,43 @@
+// -------------------- LOOP ---------------------
+void loop() {
+  customLoop();       // вызов блока своих функций (вкладка custom)
+
+  checkPID();         // пересчёт регулятора
+  backlTick();        // таймер неактивности подсветки
+  debTick();          // таймер неактивности дисплея
+  controlTick();      // управление
+  plotTick();         // суточные графики
+  readAllSensors();   // опрос датчиков
+
+  driveTick();        // работа привода
+
+#if (USE_PID_RELAY == 1)  // шим-реле
+  tickPWM_0();
+  tickPWM_1();
+#endif
+
+#if (SERVO1_RELAY == 0)
+  servo1.tick();      // здесь происходит движение серво по встроенному таймеру!
+#endif
+#if (SERVO2_RELAY == 0)
+  servo2.tick();      // здесь происходит движение серво по встроенному таймеру!
+#endif
+
+  if (currentChannel == -3) {         // если СЕРВИС
+    serviceTick();
+  } else {  // если ОТЛАДКА или НАСТРОЙКИ
+    if (millis() - commonTimer > 1000) {
+      commonTimer = millis();
+      timersTick();
+      if (currentChannel == -1) {
+        if (debugPage == 0) redrawDebug();
+        else redrawPlot();
+      }
+    }
+  }
+
+#if (WDT_ENABLE == 1)
+  wdt_reset();        // пинаем собаку
+#endif
+}
+// -------------------- LOOP ---------------------
